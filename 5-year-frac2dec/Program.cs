80c3814ccb7694afc760543294c5cedfe885b0bb
Chris Phillips <ckphillips@jackhenry.com>
2019-06-14 15:11:39 -0500

    Add project files.

---
+++ 5-year-frac2dec/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5_year_frac2dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fracPrice;
+            double decPrice;
+
+            Console.WriteLine("5 Year Treasury Note Price Convertor");
+            Console.WriteLine("This will convert a fractional price to a decimal price\n");
+            Console.Write("What is the fractional price? (###'##.##): ");
+            fracPrice = Convert.ToString(Console.ReadLine());
+            decPrice = CalculateDecimal(fracPrice);
+            Console.WriteLine("\n" + fracPrice + " converts to " + decPrice);
+        }
+
+        public double CalculateDecimal()
+
+    }
+}
